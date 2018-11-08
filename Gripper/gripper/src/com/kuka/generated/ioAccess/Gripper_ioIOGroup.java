51a683116c88f8783cd488294022a7f4c699a6ee
Roland Varga <roland.varga601@gmail.com>
2018-11-08 09:26:53 +0100

    Gripper IOConfig in SunriseOS16
    IOConfig for the gripper defining the open, close and status methods.

---
+++ Gripper/gripper/src/com/kuka/generated/ioAccess/Gripper_ioIOGroup.java
@@ -0,0 +1,139 @@
+package com.kuka.generated.ioAccess;
+
+import javax.inject.Inject;
+import javax.inject.Singleton;
+
+import com.kuka.roboticsAPI.controllerModel.Controller;
+import com.kuka.roboticsAPI.ioModel.AbstractIOGroup;
+import com.kuka.roboticsAPI.ioModel.IOTypes;
+
+/**
+ * Automatically generated class to abstract I/O access to I/O group <b>Gripper_io</b>.<br>
+ * <i>Please, do not modify!</i>
+ * <p>
+ * <b>I/O group description:</b><br>
+ * ./.
+ */
+@Singleton
+public class Gripper_ioIOGroup extends AbstractIOGroup
+{
+	/**
+	 * Constructor to create an instance of class 'Gripper_io'.<br>
+	 * <i>This constructor is automatically generated. Please, do not modify!</i>
+	 *
+	 * @param controller
+	 *            the controller, which has access to the I/O group 'Gripper_io'
+	 */
+	@Inject
+	public Gripper_ioIOGroup(Controller controller)
+	{
+		super(controller, "Gripper_io");
+
+		addDigitalOutput("Close", IOTypes.BOOLEAN, 1);
+		addDigitalOutput("Open", IOTypes.BOOLEAN, 1);
+		addInput("Status", IOTypes.BOOLEAN, 1);
+	}
+
+	/**
+	 * Gets the value of the <b>digital output '<i>Close</i>'</b>.<br>
+	 * <i>This method is automatically generated. Please, do not modify!</i>
+	 * <p>
+	 * <b>I/O direction and type:</b><br>
+	 * digital output
+	 * <p>
+	 * <b>User description of the I/O:</b><br>
+	 * ./.
+	 * <p>
+	 * <b>Range of the I/O value:</b><br>
+	 * [false; true]
+	 *
+	 * @return current value of the digital output 'Close'
+	 */
+	public boolean getClose()
+	{
+		return getBooleanIOValue("Close", true);
+	}
+
+	/**
+	 * Sets the value of the <b>digital output '<i>Close</i>'</b>.<br>
+	 * <i>This method is automatically generated. Please, do not modify!</i>
+	 * <p>
+	 * <b>I/O direction and type:</b><br>
+	 * digital output
+	 * <p>
+	 * <b>User description of the I/O:</b><br>
+	 * ./.
+	 * <p>
+	 * <b>Range of the I/O value:</b><br>
+	 * [false; true]
+	 *
+	 * @param value
+	 *            the value, which has to be written to the digital output 'Close'
+	 */
+	public void setClose(java.lang.Boolean value)
+	{
+		setDigitalOutput("Close", value);
+	}
+
+	/**
+	 * Gets the value of the <b>digital output '<i>Open</i>'</b>.<br>
+	 * <i>This method is automatically generated. Please, do not modify!</i>
+	 * <p>
+	 * <b>I/O direction and type:</b><br>
+	 * digital output
+	 * <p>
+	 * <b>User description of the I/O:</b><br>
+	 * ./.
+	 * <p>
+	 * <b>Range of the I/O value:</b><br>
+	 * [false; true]
+	 *
+	 * @return current value of the digital output 'Open'
+	 */
+	public boolean getOpen()
+	{
+		return getBooleanIOValue("Open", true);
+	}
+
+	/**
+	 * Sets the value of the <b>digital output '<i>Open</i>'</b>.<br>
+	 * <i>This method is automatically generated. Please, do not modify!</i>
+	 * <p>
+	 * <b>I/O direction and type:</b><br>
+	 * digital output
+	 * <p>
+	 * <b>User description of the I/O:</b><br>
+	 * ./.
+	 * <p>
+	 * <b>Range of the I/O value:</b><br>
+	 * [false; true]
+	 *
+	 * @param value
+	 *            the value, which has to be written to the digital output 'Open'
+	 */
+	public void setOpen(java.lang.Boolean value)
+	{
+		setDigitalOutput("Open", value);
+	}
+
+	/**
+	 * Gets the value of the <b>digital input '<i>Status</i>'</b>.<br>
+	 * <i>This method is automatically generated. Please, do not modify!</i>
+	 * <p>
+	 * <b>I/O direction and type:</b><br>
+	 * digital input
+	 * <p>
+	 * <b>User description of the I/O:</b><br>
+	 * ./.
+	 * <p>
+	 * <b>Range of the I/O value:</b><br>
+	 * [false; true]
+	 *
+	 * @return current value of the digital input 'Status'
+	 */
+	public boolean getStatus()
+	{
+		return getBooleanIOValue("Status", false);
+	}
+
+}
