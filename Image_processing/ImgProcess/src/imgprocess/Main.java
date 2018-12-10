5db844278479264b44f43517b8b54b53036d1690
Roland Varga <roland.varga601@gmail.com>
2018-12-10 18:35:32 +0100

    Image rotator function and test images were added
    Because of the rotated positioning of the camera it is necessary to rotate the taken pictures back. Some images related to the final setup were added to the TestImages folder.

--- Image_processing/ImgProcess/src/imgprocess/Main.java
+++ Image_processing/ImgProcess/src/imgprocess/Main.java
@@ -17,25 +17,41 @@ public class Main {
 		String libraryPath = "C:\\Users\\vargaro\\Documents\\general\\szakdolgozat\\Szakdolgozat\\TestImages\\";
 		String fileName = "";
 		try {
-			fileName = "calibration21.jpg";
+			fileName = "finaltest-original.jpg";
 		    img = ImageIO.read(new File(libraryPath + fileName));
 		} catch (IOException e) {
 		}
 		int verticalCorners = 9;
 		int horizontalCorners = 10;
+		img = CameraHandler.rotateClockwise90(img);
 		FindCorners cornerFinder = new FindCorners(img, horizontalCorners, verticalCorners);
 		double[][] pointCoordinates = cornerFinder.GetPointCoordinates();
+		
+/*		try {
+			fileName = "final0.jpg";
+		    img = ImageIO.read(new File(libraryPath + fileName));
+		} catch (IOException e) {
+		}
+		verticalCorners = 9;
+		horizontalCorners = 10;
+		img = CameraHandler.rotateClockwise90(img);
+		FindCorners cornerFinder2 = new FindCorners(img, horizontalCorners, verticalCorners);
+		double[][] pointCoordinates2 = cornerFinder2.GetPointCoordinates();
+		System.out.println("Number of points: " + pointCoordinates2.length);*/
+		
 		System.out.println(pointCoordinates.length);
-		BufferedImage imgbefore = null;
+/*		BufferedImage imgbefore = null;
 		try {
-			fileName = "beginning.jpg";
+			fileName = "finaltest.jpg";
 		    imgbefore = ImageIO.read(new File(libraryPath + fileName));
+		    imgbefore = CameraHandler.rotateClockwise90(imgbefore);
 		} catch (IOException e) {
 		}
 		BufferedImage imgafter = null;
 		try {
-			fileName = "white1.jpg";
+			fileName = "final2.jpg";
 			imgafter = ImageIO.read(new File(libraryPath + fileName));
+			imgafter = CameraHandler.rotateClockwise90(imgafter);
 		} catch (IOException e) {
 		}
 		ScanPiecesPosition pp = new ScanPiecesPosition(pointCoordinates, horizontalCorners, verticalCorners);
@@ -53,7 +69,7 @@ public class Main {
 				System.out.print(PiecesAtAfter[i][j] + "\t");
 			}
 			System.out.println();
-		}
+		}*/
 	}
 
 }
