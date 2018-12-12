8683e7e62a1e587d7a0857c526e3785ea6cb0523
Unknown <roland.varga601@gmail.com>
2018-12-12 15:23:27 +0100

    Some changes to generate picture for the documentation
    Some changes were applied to show the result of the greenfilter method. The final setup pictures were used for this.

--- Image_processing/ImgProcess/src/imgprocess/Main.java
+++ Image_processing/ImgProcess/src/imgprocess/Main.java
@@ -14,10 +14,10 @@ public class Main {
 	public static void main(String[] args) {
 		System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
 		BufferedImage img = null;
-		String libraryPath = "C:\\Users\\vargaro\\Documents\\general\\szakdolgozat\\Szakdolgozat\\TestImages\\";
+		String libraryPath = "images\\";
 		String fileName = "";
 		try {
-			fileName = "finaltest-original.jpg";
+			fileName = "finaltest-calibration.jpg";
 		    img = ImageIO.read(new File(libraryPath + fileName));
 		} catch (IOException e) {
 		}
@@ -28,7 +28,7 @@ public class Main {
 		double[][] pointCoordinates = cornerFinder.GetPointCoordinates();
 		
 /*		try {
-			fileName = "final0.jpg";
+			fileName = "finaltest-step.jpg";
 		    img = ImageIO.read(new File(libraryPath + fileName));
 		} catch (IOException e) {
 		}
@@ -40,30 +40,42 @@ public class Main {
 		System.out.println("Number of points: " + pointCoordinates2.length);*/
 		
 		System.out.println(pointCoordinates.length);
-/*		BufferedImage imgbefore = null;
+		BufferedImage imgbefore = null;
 		try {
-			fileName = "finaltest.jpg";
+			fileName = "finaltest-step.jpg";
 		    imgbefore = ImageIO.read(new File(libraryPath + fileName));
 		    imgbefore = CameraHandler.rotateClockwise90(imgbefore);
 		} catch (IOException e) {
 		}
-		BufferedImage imgafter = null;
+		String directoryName = "./images/images-out/";
+	    fileName = "image" + 10 + "_" + 10 + ".jpg";
+
+	    File outputfile = new File(directoryName + "/" + fileName);
+	    try{
+	    	ImageIO.write(imgbefore, "jpg", outputfile);
+	    }
+	    catch (IOException e){
+	        e.printStackTrace();
+	        System.exit(-1);
+	    }
+		
+/*		BufferedImage imgafter = null;
 		try {
 			fileName = "final2.jpg";
 			imgafter = ImageIO.read(new File(libraryPath + fileName));
 			imgafter = CameraHandler.rotateClockwise90(imgafter);
 		} catch (IOException e) {
-		}
+		}*/
 		ScanPiecesPosition pp = new ScanPiecesPosition(pointCoordinates, horizontalCorners, verticalCorners);
 		boolean[][] PiecesAtBefore = pp.ScanPositions(imgbefore);
-		boolean[][] PiecesAtAfter = pp.ScanPositions(imgafter);
+//		boolean[][] PiecesAtAfter = pp.ScanPositions(imgafter);
 		for (int i = 0; i < 8; i++) {
 			for (int j = 0; j < 8; j++) {
 				System.out.print(PiecesAtBefore[i][j] + "\t");
 			}
 			System.out.println();
 		}
-		System.out.println();
+/*		System.out.println();
 		for (int i = 0; i < 8; i++) {
 			for (int j = 0; j < 8; j++) {
 				System.out.print(PiecesAtAfter[i][j] + "\t");
